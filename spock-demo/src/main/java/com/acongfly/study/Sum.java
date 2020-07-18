601429f4652be3bd2abf75ee702cd9fb0643bbb0
shicongyang <acongfly@163.com>
2020-07-19 00:52:37 +0800

    okhttp

---
+++ spock-demo/src/main/java/com/acongfly/study/Sum.java
@@ -0,0 +1,7 @@
+package com.acongfly.study;
+
+public class Sum {
+    public int sum(int first, int second) {
+        return first + second;
+    }
+}
---
+++ studyjava/src/main/java/com/acongfly/studyjava/http/ApiRetrofit.java
@@ -0,0 +1,86 @@
+// package com.acongfly.studyjava.http;
+//
+// import okhttp3.OkHttpClient;
+// import okhttp3.logging.HttpLoggingInterceptor;
+// import org.apache.log4j.Logger;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
+// import retrofit2.Retrofit;
+// import retrofit2.converter.jackson.JacksonConverterFactory;
+//
+// import javax.annotation.PostConstruct;
+//
+// @Component
+// public class ApiRetrofit {
+//
+// private static final Logger log = Logger.getLogger(ApiRetrofit.class);
+//
+// private CommonApi commonApi;
+//
+// public CommonApi getCommonApi() {
+// return commonApi;
+// }
+//
+// private CorpApi corpApi;
+//
+// public CorpApi getCorpApi(){
+// return corpApi;
+// }
+//
+// @Value("${api.common}")
+// private String COMMON_BASE_URL ;//= "http://10.10.168.79:10100/";
+//
+// @Value("${api.corp}")
+// private String CROP_BASE_URL;
+//
+// @PostConstruct
+// public void init(){
+// HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor(new HttpLogger());
+// logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//
+// OkHttpClient client = new OkHttpClient.Builder()
+// .addNetworkInterceptor(logInterceptor)
+// .build();
+//
+// Retrofit retrofit_common = new Retrofit.Builder()
+// .baseUrl(COMMON_BASE_URL)
+// .client(client)
+// .addConverterFactory(JacksonConverterFactory.create())
+// .build();
+// commonApi = retrofit_common.create(CommonApi.class);
+//
+// Retrofit retrofit_corp = new Retrofit.Builder()
+// .baseUrl(CROP_BASE_URL+"/")
+// .client(client)
+// .addConverterFactory(JacksonConverterFactory.create())
+// .build();
+// corpApi = retrofit_corp.create(CorpApi.class);
+//
+// }
+//
+//
+// private class HttpLogger implements HttpLoggingInterceptor.Logger {
+//// private StringBuilder mMessage = new StringBuilder();
+//
+// @Override
+// public void log(String message) {
+// log.info(message);
+////
+//// if (message.startsWith("--> POST")) {
+//// mMessage.setLength(0);
+//// mMessage.append("\n");
+//// }
+//// // 以{}或者[]形式的说明是响应结果的json数据，需要进行格式化
+//// if ((message.startsWith("{") && message.endsWith("}"))
+//// || (message.startsWith("[") && message.endsWith("]"))) {
+//// message = JSONUtil.formatJson(message);
+//// }
+//// mMessage.append(message.concat("\n"));
+//// // 请求或者响应结束，打印整条日志
+//// if (message.startsWith("<-- END HTTP")) {
+//// log.info(mMessage.toString());
+//// }
+//
+// }
+// }
+// }
